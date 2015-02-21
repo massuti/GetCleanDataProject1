337540f498c3da41499619a8a9839b12b6823366
Massuti <massuti@hotmail.com>
2015-02-21 11:44:15 -0200

    fresh code

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,14 @@
+## R code for getting and cleanind data project 1
 
 #Download and unzip data:
+
 fileUrl = "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileUrl, destfile="gcd_1.zip")
                 unzip("gcd_1.zip")
 
+
 #read variables and name columns:
+
 var.labels      <- read.table("UCI HAR Dataset/features.txt")
 act.labels      <- read.table("UCI HAR Dataset/activity_labels.txt")
 testid          <- read.table("UCI HAR Dataset/test/subject_test.txt")
@@ -23,15 +27,20 @@ colnames(trainid)       <- "subID"
 colnames(act.labels)    <- c("actID","actType")
 
 
+
 #Now with the requested code:
 #1 Merges the training and the test sets to create one 
 #  data set.
+
+
 tests <- cbind(testid, testy, testx)
 trains <- cbind(trainid, trainy, trainx)
 total <- rbind(tests, trains)
 
+
 #2 Extracts only the measurements on the mean and standard
 #  deviation for each measurement. 
+
 
 var.names <- colnames(total)
 
@@ -42,14 +51,18 @@ logvect = (grepl("..ID", var.names) |
 
 finaldata <- total[logvect==TRUE]
 
+
 #3 Uses descriptive activity names to name the activities
 #  in the data set
+
 
 finaldata = merge(act.labels, finaldata,by="actID",
                   all.x=TRUE)
 
+
 #4 Appropriately labels the data set with descriptive 
 #  variable names. 
+
 
 var.names  = colnames(finaldata)
 
@@ -71,9 +84,11 @@ for (i in 1:length(var.names))
 
 colnames(finaldata) <- var.names
 
+
 #5 From the data set in step 4, creates a second, 
 #  independent tidy data set with the average of each variable 
 #  for each activity and each subject.
+
 
 library(plyr)
 
